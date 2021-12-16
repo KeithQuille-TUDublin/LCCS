887768970463c0e23a49c65f334fd56b125e50b3
Keith Quille <72391746+KeithQuille-TUDublin@users.noreply.github.com>
2021-12-16 20:44:48 +0000

    Update testCode.js

--- testCode.js
+++ testCode.js
@@ -1,1 +1,17 @@
+<!-- Firebase - like import-->
+<script src="https://www.gstatic.com/firebasejs/8.0.0/firebase.js"></script>
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 
+<!-- firebase connection -like myDb -->
+<script>
+const firebaseConfig = {
+  apiKey: "AIzaSyD9mB9HeU2AARcd7u6HsKMmNHkjcsUEetw",
+  authDomain: "livecmbarra.firebaseapp.com",
+  projectId: "livecmbarra",
+  storageBucket: "livecmbarra.appspot.com",
+  messagingSenderId: "465942013952",
+  appId: "1:465942013952:web:a02a3541c0b5f3064dd0e5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+</script>
